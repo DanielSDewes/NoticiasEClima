bfe205b4925afe2634ab12b3c39d82e4fc3ddf66
DanielD <danielsilvadewe@gmail.com>
2025-08-10 20:04:58 -0300

    ajustando logo e permitindo requisição do front no middleware

--- main.py
+++ main.py
@@ -92,11 +92,14 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://noticias-e-clima.vercel.app",
+    "http://localhost:3000",], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.post("/register", response_model=UserOut)
 async def register(user: UserCreate):
