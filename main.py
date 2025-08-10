b343d52c0582597e358f82183bebbaeb333b423b
DanielD <danielsilvadewe@gmail.com>
2025-08-10 20:15:33 -0300

    ajustando aplicação novamente

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://noticias-e-clima-fuauv6vpi-danielsdewes-projects.vercel.app/",
+    "https://noticias-e-clima.vercel.app/",
     "http://localhost:3000",], 
     allow_credentials=True,
     allow_methods=["*"],
