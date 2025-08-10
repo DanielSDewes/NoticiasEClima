c343ee034cdb87b51dca3de3a2893a90413dc908
DanielD <danielsilvadewe@gmail.com>
2025-08-10 20:18:01 -0300

    link local

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "https://noticias-e-clima.vercel.app/",
-    "http://localhost:3000",], 
+    "http://127.0.0.1:5500/",], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
